fe174f7672abfcdc918fa870c4c727c91a705f54
frachix <fathirachid995@gmail.com>
2026-09-24 17:46:56 +0100

    Add additional print statements to HelloRachid

--- src/day_02/HelloRachid.java
+++ src/day_02/HelloRachid.java
@@ -4,5 +4,8 @@ public class HelloRachid {
 
     public static void main(String[] args) {
         System.out.println("Cydeo");
+        System.out.println("HelloRachid");
+        System.out.println("HelloBro");
+        System.out.println("Chelsea");
     }
 }
